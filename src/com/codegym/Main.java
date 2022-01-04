959309b521c59d44916c20ceb365ee57b064b01a
ruukichi <ngovietlc93@gmail.com>
2022-01-04 16:54:56 +0700

    tinh chi so bmi

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,28 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.print("Cân nặng của bạn: ");
+        weight = scanner.nextDouble();
+
+        System.out.print("Chiều cao của bạn: ");
+        height = scanner.nextDouble();
+        bmi = weight / Math.pow(height, 2);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+
+        if (bmi < 18)
+            System.out.printf("%-10.1f%s", bmi, "Underweight");
+        else if (bmi < 25.0)
+            System.out.printf("%-10.1f%s", bmi, "Normal");
+        else if (bmi < 30.0)
+            System.out.printf("%-10.1f%s", bmi, "Overweight");
+        else
+            System.out.printf("%-10.1f%s", bmi, "Obese");
+    }
+}
+
